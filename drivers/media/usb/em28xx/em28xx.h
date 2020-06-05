5bd2ec92c9df27cba39be0406fa960d285b59150
Brad Love <brad@nextdimension.cc>
2020-06-05 16:30:52 -0500

    em28xx: Add support for Hauppauge USB QuadHD
    Hauppauge USB QuadHD contains two two-tuner em28xx devices behind a usb hub.
    Each of the four dvb adapters contains a MaxLinear 692 combo ATSC demod/tuner.
    
    Signed-off-by: Brad Love <brad@nextdimension.cc>

--- drivers/media/usb/em28xx/em28xx.h
+++ drivers/media/usb/em28xx/em28xx.h
@@ -151,6 +151,7 @@
 #define EM2882_BOARD_ZOLID_HYBRID_TV_STICK		102
 #define EM2861_BOARD_MAGIX_VIDEOWANDLER2          103
 #define EM28178_BOARD_PCTV_461E_V2                104
+#define EM2874S_BOARD_HAUPPAUGE_USB_QUADHD        105
 
 /* Limits minimum and default number of buffers */
 #define EM28XX_MIN_BUF 4
